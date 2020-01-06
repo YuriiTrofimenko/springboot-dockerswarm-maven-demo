2a98389167091d87864440f8e22a472b3261f00e
YuriiTrofimenko <tyaa@ukr.net>
2020-01-06 15:55:19 +0200

    init n create RestController

---
+++ src/main/kotlin/org/tyaa/springbootdockerswarm/SpringbootdockerswarmApplication.kt
@@ -0,0 +1,11 @@
+package org.tyaa.springbootdockerswarm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootdockerswarmApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootdockerswarmApplication>(*args)
+}
---
+++ src/test/kotlin/org/tyaa/springbootdockerswarm/SpringbootdockerswarmApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.tyaa.springbootdockerswarm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootdockerswarmApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
